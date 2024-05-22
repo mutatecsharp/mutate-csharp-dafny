7a3c98a5399758247e542a2603fe81a9be869092
Khemi <jiajinchew@gmail.com>
2024-05-22 15:13:24 +0000

    chore: add script to delete flaky tests

---
+++ delete-flaky-tests.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -uex
+
+test -f env.sh && echo "mutate-csharp-dafny/env.sh found" || { echo "mutate-csharp-dafny/env.sh not found"; exit 1; }
+source env.sh
+
+EXPERIMENT=false
+
+while getopts "eh" opt; do
+    case $opt in
+        e)
+            EXPERIMENT=true
+            ;;
+        h)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+TESTCASE=$1
+
+# Locate dafny path based on the experiment flag
+if $EXPERIMENT; then
+    DAFNY_PROJECT_PATH="$TESTBENCH/dafny"
+else
+    DAFNY_PROJECT_PATH="$WORKSPACE/dafny"
+fi
+
+pushd "$DAFNY_PROJECT_PATH"
+
+DAFNY_INTEGRATION_TEST_PATH="$DAFNY_PROJECT_PATH/Source/IntegrationTests/TestFiles/LitTests/LitTest"
+test -d "$DAFNY_INTEGRATION_TEST_PATH"
+
+# Delete flaky integration tests 
+
+# projectAsLibrary
+rm "$DAFNY_INTEGRATION_TEST_PATH/cli/projectFile/libs/high/projectAsLibrary.dfy"
+rm "$DAFNY_INTEGRATION_TEST_PATH/cli/projectFile/libs/high/projectAsLibrary.dfy.expect"
+
+# SequenceRace
+rm "$DAFNY_INTEGRATION_TEST_PATH/benchmarks/sequence-race/SequenceRace.dfy"
+rm "$DAFNY_INTEGRATION_TEST_PATH/benchmarks/sequence-race/SequenceRace.dfy.expect"
+
+# git-issues/git-issue-1514.dfy
+rm "$DAFNY_INTEGRATION_TEST_PATH/git-issues/git-1514.dfy"
+rm "$DAFNY_INTEGRATION_TEST_PATH/git-issues/git-1514.dfy.expect"
+rm "$DAFNY_INTEGRATION_TEST_PATH/git-issues/git-1514.dfy.rs.check"
+
+# git-issues/git-issue-1514c.dfy
+rm "$DAFNY_INTEGRATION_TEST_PATH/git-issues/git-issue-1514c.dfy"
+rm "$DAFNY_INTEGRATION_TEST_PATH/git-issues/git-issue-1514c.dfy.expect"
+rm "$DAFNY_INTEGRATION_TEST_PATH/git-issues/git-issue-1514c.dfy.rs.check"
+
+# git-issues/git-issue-267.dfy
+rm "$DAFNY_INTEGRATION_TEST_PATH/git-issues/git-267.dfy"
+rm "$DAFNY_INTEGRATION_TEST_PATH/git-issues/git-267.dfy.expect"
+rm "$DAFNY_INTEGRATION_TEST_PATH/git-issues/git-267.dfy.py.check"
