7cd4b2407ec57349af8f9fe14465a3fcc32b5dac
Khemi <jiajinchew@gmail.com>
2024-05-22 14:28:40 +0100

    fix: use getopts to get experiment flag

--- generate-patch.sh
+++ generate-patch.sh
@@ -3,11 +3,31 @@
 set -uex
 
 # Should source the env.sh file in mutate-csharp project before running this script
-test -f env.sh
+test -f env.sh && echo "mutate-csharp-dafny/env.sh found" || { echo "mutate-csharp-dafny/env.sh not found"; exit 1; }
 source env.sh
 
+# Get options to determine if we want to generate patch for experiments or SUT
+usage() {
+    echo "Usage: $0 [-e]"
+    exit 1
+}
+
+EXPERIMENT=false
+
+while getopts "eh" opt; do
+    case $opt in
+        e)
+            EXPERIMENT=true
+            ;;
+        h)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
 # Set the artifact path
-if [ -n "$1" ] && [ "$1" == "experiment" ]; then
+if $EXPERIMENT; then
     ARTIFACT_PATH="$EXPERIMENT_ARTIFACT_PATH"
     DAFNY_PROJECT_PATH="$TESTBENCH/dafny"
     PATCH_FILE="$WORKSPACE/experiment-artifact-output.patch"
