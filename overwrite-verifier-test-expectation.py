24e88d15bdd01ed15bf34f3058989137029a4f00
Khemi <jiajinchew@gmail.com>
2024-05-26 20:20:59 +0100

    fix: add required whitespace

--- overwrite-verifier-test-expectation.py
+++ overwrite-verifier-test-expectation.py
@@ -34,7 +34,7 @@ def obtain_env_vars():
 
 def replace_lines_in_file(file_path):
     # Define the pattern with capturing groups for the parts to be preserved
-    basic_pattern = re.compile(r'(Dafny program verifier finished with )\d+( verified, )\d+(errors?)')
+    basic_pattern = re.compile(r'(Dafny program verifier finished with )\d+( verified, )\d+( errors?)')
     check_pattern = re.compile(r'(// CHECK: .*Dafny program verifier finished with )\d+( verified, )\d+( errors.*)')
 
     def basic_replacement(match):
