66449f471ee20f323a79836849609ab726c3be9d
Khemi <jiajinchew@gmail.com>
2024-06-01 07:16:36 +0100

    feat: implement mutation-guided fuzzer test generation

--- env.sh
+++ env.sh
@@ -6,6 +6,7 @@ set -uex
 # Volume root should be set
 test -d "$VOLUME_ROOT"
 
+REGULAR_ROOT="$VOLUME_ROOT/original"
 MUTATED_ROOT="$VOLUME_ROOT/workspace"
 TRACED_ROOT="$VOLUME_ROOT/testbench"
 
@@ -15,21 +16,25 @@ MUTATE_DAFNY_RECORDS_ROOT="$VOLUME_ROOT/mutate-dafny-records"
 MUTATED_ARTIFACT_PATH="$MUTATED_ROOT/testartifact"
 TRACED_ARTIFACT_PATH="$TRACED_ROOT/testartifact"
 
+REGULAR_DAFNY_ROOT="$REGULAR_ROOT/dafny"
 MUTATED_DAFNY_ROOT="$MUTATED_ROOT/dafny"
 TRACED_DAFNY_ROOT="$TRACED_ROOT/dafny"
 
 test -d "$WORKSPACE_MUTATE_CSHARP_ROOT"
 test -d "$MUTATED_DAFNY_ROOT"
 test -d "$TRACED_DAFNY_ROOT"
+test -d "$REGULAR_DAFNY_ROOT"
 
 echo "mutate-dafny-records directory: $MUTATE_DAFNY_RECORDS_ROOT"
 echo "mutate-csharp directory: $WORKSPACE_MUTATE_CSHARP_ROOT"
 echo "mutated Dafny directory: $MUTATED_DAFNY_ROOT"
 echo "traced Dafny directory: $TRACED_DAFNY_ROOT"
+echo "regular Dafny directory: $REGULAR_DAFNY_ROOT"
 
 export VOLUME_ROOT
 export WORKSPACE
 export TESTBENCH
+export REGULAR_DAFNY_ROOT
 export MUTATED_DAFNY_ROOT
 export TRACED_DAFNY_ROOT
 export MUTATED_ARTIFACT_PATH
--- fuzzing/run-fuzz-d.py
+++ fuzzing/run-fuzz-d.py
@@ -85,6 +85,10 @@ def time_budget_exists(test_campaign_start_time_in_seconds: float,
                        test_campaign_budget_in_hours: int):
     time_budget_in_seconds = test_campaign_budget_in_hours * 3600
     elapsed_time_in_seconds = int(time.time() - test_campaign_start_time_in_seconds)
+
+    elapsed_timedelta = timedelta(seconds=elapsed_time_in_seconds)
+    print(f"Test campaign elapsed time: {str(elapsed_timedelta)}")
+
     return elapsed_time_in_seconds < time_budget_in_seconds
 
 
@@ -115,18 +119,17 @@ def execute_dafny(dafny_binary: Path,
     (exit_code, stdout, stderr, timeout) = run_subprocess(compile_command, timeout_in_seconds)
     elapsed_time = time.time() - start_time
 
-    # todo: convert stdout/stderr to string instead of bytes
     if exit_code != 0:
-        print(f"""Skipping: error while compiling generated. ({dafny_type.name})
+        print(f"""Skipping: error while compiling fuzz-d generated program. ({dafny_type.name})
         Standard output:
-        {stdout}
+        {stdout.decode('utf-8')}
         Standard error:
-        {stderr}
+        {stderr.decode('utf-8')}
         """)
         return False, None
 
     if timeout:
-        print(f"Skipping: timeout while compiling generated program. ({dafny_type.name})")
+        print(f"Skipping: timeout while compiling fuzz-d generated program. ({dafny_type.name})")
         return False, None
 
     if not executable_output_path.is_file():
@@ -225,6 +228,9 @@ def mutation_guided_test_generation(fuzz_d_binary: Path,
                                     execution_timeout_in_seconds: int):
     # Mutation testing
     killed_mutants: set = set()  # set of (file_env_var, mutant_id)
+    # Interesting mutants to generate tests against:
+    # 1) Mutants that are not covered by any tests
+    # 2) Mutants that are covered by at least one test but survive / passes all tests when activated
     surviving_mutants: set = set()  # todo: update with candidates
 
     time_of_last_kill = time.time()  # in seconds since epoch
@@ -257,7 +263,7 @@ def mutation_guided_test_generation(fuzz_d_binary: Path,
                 test_campaign_start_time_in_seconds=test_campaign_start_time,
                 test_campaign_budget_in_hours=test_campaign_budget_in_hours) and len(surviving_mutants) > 0:
             fuzz_d_fuzzer_seed = random.randint(0, LONG_UPPER_BOUND)
-            program_uid = f"fuzzd-{fuzz_d_fuzzer_seed}"
+            program_uid = f"fuzzd_{fuzz_d_fuzzer_seed}" # note: seed can be negative?
             current_program_output_dir = mutation_testing_artifact_dir / program_uid
             current_mutation_testing_program_path = current_program_output_dir / 'valid.dfy'
 
@@ -266,7 +272,18 @@ def mutation_guided_test_generation(fuzz_d_binary: Path,
                 print(f"Skipping: another runner is working on the same seed ({fuzz_d_fuzzer_seed}).")
                 continue
 
-            # todo: delete compilations
+            # 0) Delete generated files from previous iteration.
+            if fuzz_d_output_path.exists():
+                fuzz_d_output_path.unlink()
+            if mutant_trace_file_path.exists():
+                mutant_trace_file_path.unlink()
+            if default_compiled_executable_path.exists():
+                default_compiled_executable_path.unlink()
+            if mutated_compiled_executable_path.exists():
+                mutated_compiled_executable_path.unlink()
+            if traced_compiled_executable_path.exists():
+                traced_compiled_executable_path.unlink()
+
             # 1) Generate a valid Dafny program
             if not execute_fuzz_d(fuzz_d_binary,
                                   fuzz_d_generation_dir,
@@ -332,12 +349,10 @@ def mutation_guided_test_generation(fuzz_d_binary: Path,
                 print(f"Skipping: execution trace is corrupted. (seed: {fuzz_d_fuzzer_seed}))")
                 continue
 
-            # todo: get env var for particular source file under test
             # 7) Filter for particular source file under test if specified and discard killed mutants from consideration.
-            source_file_under_test_env_var = None
-            if source_file_under_test_env_var is not None:
+            if source_file_env_var is not None:
                 mutants_covered_by_program = [(env_var, mutant_id) for (env_var, mutant_id) in
-                                              mutants_covered_by_program if env_var == source_file_under_test_env_var]
+                                              mutants_covered_by_program if env_var == source_file_env_var]
             candidate_mutants_for_program = [(env_var, mutant_id) for (env_var, mutant_id) in mutants_covered_by_program
                                              if (env_var, mutant_id) not in killed_mutants]
 
@@ -345,7 +360,8 @@ def mutation_guided_test_generation(fuzz_d_binary: Path,
                 f"Number of mutants covered by generated program with seed {fuzz_d_fuzzer_seed}: {len(mutants_covered_by_program)}")
 
             # Sort mutants: since mutants that are sequential in ID are likely to belong in the same mutation group,
-            # killing one mutant in the mutation group might lead to kills in the other mutants in the same mutation group.
+            # killing one mutant in the mutation group might lead to kills in the other mutants in the same mutation
+            # group.
             mutants_covered_by_program.sort()
 
             # 8) Perform mutation testing on the generated Dafny program with the mutated Dafny compiler.
@@ -467,43 +483,103 @@ def main():
         print('Volume directory not found. Please set VOLUME_ROOT environment variable.')
         exit(1)
 
-    # todo: use defaults
     env = obtain_env_vars()
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--seed', type=int, help='Seed for random number generator.')
-    parser.add_argument("--fuzz-d", type=str, required=True, help='Path to the fuzz-d binary.',
-                        default=f"{os.getcwd()}/third_party/fuzz-d/app/build/libs/app.jar")
-    parser.add_argument("--dafny", type=str, required=True, help='Path to the non-mutated Dafny binary.')
-    parser.add_argument("--mutated-dafny", type=str, required=True, help='Path to the mutated Dafny binary.')
-    parser.add_argument("--traced-dafny", type=str, required=True,
-                        help='Path to the execution-trace instrumented Dafny binary.')
-    parser.add_argument("--output-directory", type=str, required=True,
+    parser.add_argument("--dry-run", action='store_true',
+                        help="Perform dry run.")
+    parser.add_argument('--seed', type=int,
+                        help='Optional. Seed for random number generator.')
+    parser.add_argument("--fuzz_d", type=str,
+                        help='Path to the fuzz-d project.')
+    parser.add_argument("--dafny", type=str,
+                        help='Path to the non-mutated Dafny project.')
+    parser.add_argument("--mutated_dafny", type=str,
+                        help='Path to the mutated Dafny project.')
+    parser.add_argument("--traced_dafny", type=str,
+                        help='Path to the execution-trace instrumented Dafny project.')
+    parser.add_argument("--output_directory", type=str, required=True,
                         help='Path to the persisted/temporary interesting programs output directory.')
-    parser.add_argument('--mutation-registry', type=str, required=True,
+    parser.add_argument('--mutation_registry', type=str,
                         help='Path to registry generated after mutating the Dafny codebase.')
-    parser.add_argument('--tracer-registry', type=str, required=True,
+    parser.add_argument('--tracer_registry', type=str, required=True,
                         help='Path to registry generated after instrumenting the Dafny codebase to trace mutant executions.')
-    parser.add_argument('--compilation-timeout', default=30,
+    parser.add_argument('--source_file_relative_path', type=str,
+                        help="Optional. If specified, only consider mutants for the specified file.")
+    parser.add_argument('--compilation_timeout', default=30,
                         help='Maximum second(s) allowed to compile generated program with the non-mutated Dafny compiler.')
-    parser.add_argument('--generation-timeout', default=30,
+    parser.add_argument('--generation_timeout', default=30,
                         help='Maximum second(s) allowed to generate program with fuzz-d.')
-    parser.add_argument('--execution-timeout', default=30,
+    parser.add_argument('--execution_timeout', default=30,
                         help='Maximum second(s) allowed to execute fuzz-d generated program compiled by the non-mutated Dafny compiler.')
-    parser.add_argument('--test-campaign-timeout', default=12,
+    parser.add_argument('--test_campaign_timeout', default=12,
                         help='Test campaign time budget in hour(s).')
 
     # CLI arguments
     args = parser.parse_args()
 
-    fuzz_d_binary_path = Path(args.fuzz_d).absolute()
-    dafny_binary_path = Path(args.dafny).absolute()
-    mutated_dafny_binary_path = Path(args.mutated_dafny).absolute()
-    traced_dafny_binary_path = Path(args.traced_dafny).absolute()
-    artifact_directory = Path(args.output_directory).absolute()
+    # Set defaults
+    if args.fuzz_d is not None:
+        fuzz_d_root = Path(args.fuzz_d).resolve()
+    else:
+        fuzz_d_root = Path(f"{os.getcwd()}/third_party/fuzz-d")
 
-    if args.seed is not None:
-        random.seed(args.seed)
+    # Build fuzz-d
+    fuzz_d_binary_path = fuzz_d_root / "app" / "build" / "libs" / "app.jar"
+    os.system(f"cd {fuzz_d_root} && ./gradlew build && cd -")
+
+    # Note: we use the provided script to execute Dafny as this is suggested in the Dafny README.
+    # (https://github.com/dafny-lang/dafny/wiki/INSTALL)
+    # The script is a simple wrapper around the Dafny executable that handles cross-OS nuances.
+    # Regular dafny
+    if args.dafny is not None:
+        regular_dafny_dir = Path(args.dafny).resolve()
+    else:
+        regular_dafny_dir = Path(f"{env['REGULAR_DAFNY_ROOT']}")
+
+    dafny_binary_path = regular_dafny_dir / "Scripts" / "Dafny"
+
+    # Mutated dafny
+    if args.mutated_dafny is not None:
+        mutated_dafny_dir = Path(args.mutated_dafny).resolve()
+    else:
+        mutated_dafny_dir = Path(f"{env['MUTATED_DAFNY_ROOT']}")
+
+    mutated_dafny_binary_path = mutated_dafny_dir / "Scripts" / "Dafny"
+
+    # Tracer dafny
+    if args.traced_dafny is not None:
+        traced_dafny_dir = Path(args.traced_dafny).resolve()
+    else:
+        traced_dafny_dir = Path(f"{env['TRACED_DAFNY_ROOT']}")
+
+    traced_dafny_binary_path = traced_dafny_dir / "Scripts" / "Dafny"
+
+    if args.mutation_registry is not None:
+        mutation_registry_path = Path(args.mutation_registry).resolve()
+    else:
+        mutation_registry_path = Path(f"{env['MUTATED_DAFNY_ROOT']}") / "Source" / "DafnyCore" / "registry.mucs.json"
+
+    if args.tracer_registry is not None:
+        tracer_registry_path = Path(args.tracer_registry).resolve()
+    else:
+        tracer_registry_path = Path(f"{env['TRACED_DAFNY_ROOT']}") / "Source" / "DafnyCore" / "tracer-registry.mucs.json"
+
+    mutation_registry: MutationRegistry = validated_mutant_registry(str(mutation_registry_path), str(tracer_registry_path))
+
+    source_file_env_var = None
+    if args.source_file_relative_path is not None:
+        to_find = [registry['EnvironmentVariable'] for path, registry in mutation_registry.file_relative_path_to_registry.items()
+                   if path == args.source_file_relative_path]
+        if len(to_find) == 0:
+            print("Cannot find the specified file in mutation registry.")
+            exit(1)
+        elif len(to_find) > 1:
+            print("Found more than one match for the specified file in mutation registry. Mutation registry may be corrupted.")
+            exit(1)
+        source_file_env_var = to_find[0]
+
+    artifact_directory = Path(args.output_directory).absolute()
 
     if not fuzz_d_binary_path.is_file() or \
             not dafny_binary_path.is_file() or \
@@ -511,16 +587,32 @@ def main():
             not traced_dafny_binary_path.is_file():
         print("Invalid fuzz-d or dafny binary executable paths.")
 
-    mutation_registry: MutationRegistry = validated_mutant_registry(args.mutation_registry, args.tracer_registry)
-
     # Create output directory if it does not exist
-    # artifact_directory.mkdir(parents=True, exist_ok=True)
     compilation_artifact_dir = artifact_directory / "compilations"
     mutation_testing_artifact_dir = artifact_directory / 'mutation_testing'
     killed_mutants_artifact_dir = artifact_directory / 'killed_mutants'
+
+    print(f"fuzz-d project root: {fuzz_d_root}")
+    print(f"regular dafny project root: {regular_dafny_dir}")
+    print(f"mutated dafny project root: {mutated_dafny_dir}")
+    print(f"traced dafny project root: {traced_dafny_dir}")
+    print(f"compilation artifact output directory: {compilation_artifact_dir}")
+    print(f"mutation testing artifact output directory: {mutation_testing_artifact_dir}")
+    print(f"killed mutants artifact output directory: {killed_mutants_artifact_dir}")
+
+    if source_file_env_var is not None:
+        print(f"Specified file: {args.source_file_relative_path} | Env var: {source_file_env_var}")
+
+    if args.dry_run:
+        print("Dry run complete.")
+        exit(0)
+
     compilation_artifact_dir.mkdir(parents=True, exist_ok=True)
     mutation_testing_artifact_dir.mkdir(parents=True, exist_ok=True)
     killed_mutants_artifact_dir.mkdir(parents=True, exist_ok=True)
+
+    if args.seed is not None:
+        random.seed(args.seed)
 
     mutation_guided_test_generation(fuzz_d_binary=fuzz_d_binary_path,
                                     default_dafny_binary=dafny_binary_path,
@@ -530,9 +622,11 @@ def main():
                                     mutation_testing_artifact_dir=mutation_testing_artifact_dir,
                                     killed_mutants_artifact_dir=killed_mutants_artifact_dir,
                                     mutation_registry=mutation_registry,
+                                    source_file_env_var=source_file_env_var,
                                     test_campaign_budget_in_hours=args.test_campaign_timeout,
                                     generation_budget_in_seconds=args.generation_timeout,
-                                    default_compilation_timeout_in_seconds=args.compilation_timeout)
+                                    compilation_timeout_in_seconds=args.compilation_timeout,
+                                    execution_timeout_in_seconds=args.execution_timeout)
 
 
 if __name__ == '__main__':
