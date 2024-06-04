ac811bf060c3216b3efd81ea60de05fc6b497563
Khemi <jiajinchew@gmail.com>
2024-06-04 21:24:21 +0100

    fix: typo

--- dependency_env.sh
+++ dependency_env.sh
@@ -4,7 +4,7 @@ set -uex
 
 JAVA_19_BINARY_PATH="/usr/lib/jvm/java-19-amazon-corretto/bin/java"
 
-test -f "$JAVA_JAVA_19_BINARY_PATH"
+test -f "$JAVA_19_BINARY_PATH"
 
 echo "java 19 found: $JAVA_19_BINARY_PATH"
 
