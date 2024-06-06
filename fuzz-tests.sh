195ba5181f5ee04f7f4e884c64b5cc037d71c6dc
Khemi <jiajinchew@gmail.com>
2024-06-06 04:30:53 +0100

    chore: supply mutation analysis results of multiple files

--- fuzz-tests.sh
+++ fuzz-tests.sh
@@ -41,7 +41,6 @@ test -f third_party/fuzz-d/run.py && echo "fuzz-d cloned" || { echo "fuzz-d not 
 test -f third_party/fuzz-d/app/src/main/antlr/dafny.g4 && echo "fuzz-d submodules cloned" || { echo "fuzz-d submodules not found"; exit 1; }
 
 MUTATE_CSHARP_PATH="$WORKSPACE_MUTATE_CSHARP_ROOT"
-MUTATION_ANALYSIS_PATH="$MUTATED_DAFNY_ROOT/Source/IntegrationTests/mutation-testing.mucs.json"
 
 test -d "$MUTATE_CSHARP_PATH"
 
@@ -69,18 +68,25 @@ WORKER_COUNT=32
 if [ $ONLY_TEST_UNCOVERED ]; then
   echo "only fuzz mutants unreachable by regression test suite."
   $FUZZER_SCRIPT $DRY_RUN \
-  --source_file_relative_path "$SOURCE_FILE_UNDER_TEST" \
+  --source_file_relative_path \
+  "Backends/SinglePassCodeGenerator.cs" \
+  "Backends/CSharp/CsharpCodeGenerator.cs" \
+  "Backends/JavaScript/JavaScriptCodeGenerator.cs" \
   --passing_tests "$MUTATE_DAFNY_RECORDS_ROOT/passing-tests.txt" \
   --regression_test_trace_dir "$TRACED_ARTIFACT_PATH/execution-trace" \
   --output_directory "$FUZZER_OUTPUT_DIR"
 else
-  # Sanity check: repository mutation analysis recorded
-  test -f "$MUTATION_ANALYSIS_PATH"
   echo "fuzz all survived mutants."
   for _ in $(seq 1 $WORKER_COUNT); do
     $FUZZER_SCRIPT $DRY_RUN \
-    --source_file_relative_path "$SOURCE_FILE_UNDER_TEST" \
+    --source_file_relative_path \
+    "Backends/SinglePassCodeGenerator.cs" \
+    "Backends/Csharp/CSharpCodeGenerator.cs" \
+    "Backends/JavaScript/JavaScriptCodeGenerator.cs" \
     --output_directory "$FUZZER_OUTPUT_DIR" \
-    --mutation_test_result "$MUTATION_ANALYSIS_PATH" \
+    --mutation_test_result \
+    "$MUTATE_DAFNY_RECORDS_ROOT/mutation-test/CsharpCodeGenerator/mutation-testing.mucs.json" \
+    "$MUTATE_DAFNY_RECORDS_ROOT/mutation-test/JavaScriptCodeGenerator/mutation-testing.mucs.json" \
+    "$MUTATE_DAFNY_RECORDS_ROOT/mutation-test/SinglePassCodeGenerator/mutation-testing.mucs.json" \
   & done
 fi
